3ccf9394ad29876cd252d35b8561bf9449b918d1
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-25 05:09:50 +0500

    Add files via upload

---
+++ qno20.ts
@@ -0,0 +1,10 @@
+// 20. Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let countries: string[] = [
+    "United States", "Canada", "Brazil","India", "Australia"];
+
+console.log("List of countries:", countries);
+let mountains: string[] = ["mount Everest", "K2", "Nanga Parbat","Tirch Mir"]
+console.log("List of mountains:", mountains);
+let rivers: string[] = [" Indus River", "Nail River", "Ravi River", "Jehlam River"];
+console.log("list of rivers:",rivers)
